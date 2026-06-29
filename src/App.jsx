579e0c417e100d4c9db9a075ee219fc7d206e921
mehdichamam <mehdichamam465@gmail.com>
2026-06-29 20:45:25 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-white">
+        React + Tailwind Works 🚀
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
